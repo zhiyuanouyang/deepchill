4dbd50310acf2643e73337ddb234bc374da5fdb9
Zhiyuan Ouyang <tiancaioyzy@gmail.com>
2026-09-15 17:32:42 -0700

    renew the project

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
-
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: '*.deepchill.app',
-            },
-            {
-                protocol: 'https',
-                hostname: 'interviewgpt.deepchill.app',
-            },
-        ],
-    },
+  /* config options here */
 };
 
 export default nextConfig;
